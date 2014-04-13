48f4e92e0837c25cac7ef84328d3d6f00b3d1371
void-null <void-null@users.noreply.github.com>
2014-04-13 18:25:44 +0400

    Added 4chan JSON API support

--- chin.c
+++ chin.c
@@ -6,10 +6,10 @@
 
 int main(int argc, char *argv[]){
 
-	/* Disable line buffering */
+	/* Disables line buffering */
 	setbuf(stdout, NULL);
 
-	/* Get location of home folder */
+	/* Gets location of home folder */
 	char *home = getenv("HOME");
 	char text[LENGTH]={},buff[LENGTH]={},postnum[LENGTH];
 
@@ -19,103 +19,125 @@ int main(int argc, char *argv[]){
 			"Usage: chin URL DIR\n\nDownloads images off specified 4chan thread to ~/chin/DIR\n");
 		return 0;
 	} 
-
-	/* Forms initial wget command without the link to thread */
-	sprintf(buff,"wget -N -q -P %s/chin/ ", home); 
-
-	/* Writes first argument (download link) to 'text' */
-	strcat(text,argv[1]);   
-
-	/* Appends download link to the end of wget command */
-	strcat(buff,text);     
+	/* Writes second argument to 'text' */
+	strcat(text,argv[1]);  
 
 	int j=0,thrlen=strlen(text),i,count=0;
-	i=thrlen-1;
+	
+	
+	/*I don't like those two blocks, will replace them sometime in the future */	
+		
+		/* Extracts thread number and writes it to 'postnum' */
+		i=thrlen-1;
+		while (text[i]!='/'){
+			count++;
+			i--;
+		} 
+		i=0;
+		while (count>=0){
+			postnum[i]=text[thrlen-count];
+			i++;
+			count--;
+		}
+		
+		 /* Extracts board's name */
+		char boardname[LENGTH]={};
+		j=strlen(postnum);
+		i=thrlen-6-j;
+		count=0;
+		while (text[i]!='/')
+		{
+			count++;
+			i--;
+		}
+		i=0; 
+		while (count-1>=0){
+			boardname[i]=text[thrlen-count-j-5];
+			i++;
+			count--;
+		}
+		
+	printf("\nDownloading thread  #%s on /%s/, please wait...",postnum,boardname); 
+	
+	/* Forms initial wget command */
+	sprintf(buff,"wget -N -q -P %s/chin/%s/ ", home, argv[2]); 
+ 
+	/* Appends thread link to the end of wget command */
+	strcat(buff,text);     
 
-	/* Counts amount of symbols before '/',
-	 * resulting amount is thread number's length
-	 */
-	while (text[i]!='/'){
-		count++;
-		i--;
-	} 
-	i=0;
-
-	/* Extracts thread number and writes it to 'postnum' */
-	while (count>=0){
-		postnum[i]=text[thrlen-count];
-		i++;
-		count--;
-	}
-
-	printf("\nThe thread number is: %s",postnum);
-
-	/* Downloads page source using resulting wget command */
+	/* Appends .json to the end, part of API integration */
+	strcat(buff,".json");
+	
+	/* Downloads page using resulting wget command */
 	system(buff);
 
-	printf("\nThe page source has been (hopefully) downloaded. Please wait...");
-
 	/* Writes path to downloaded file to 'buff' */
-	sprintf(buff,"%s/chin/%s",home,postnum);
+	sprintf(buff,"%s/chin/%s/%s.json",home, argv[2], postnum);
 
 	/* Opens downloaded file */
 	FILE *f=fopen(buff,"r"); 
-
+	
 	/* Writes entire file to char array 'whole' */
 	char whole[PAGE_LENGTH];
 	fgets(whole,PAGE_LENGTH,f);
-
-	/* Removes webpage file */
-	sprintf(buff,"rm %s/chin/%s",home,postnum);
-	system(buff);
-
-	char link[LENGTH]={};
+	
 	char wget[LENGTH]={};
+	char extension[LENGTH]={};
+	char imagenumber[LENGTH]={};
 
-	/* i is starting offset in page's HTML code, to be changed */
-	i=20;
+	/* i is starting offset */
+	i=10;
 
 	/* Resets counter */
 	count=0;
 
 	/* Writes path to download folder to 'buff' */
 	sprintf(buff,"%s/chin/%s",home,argv[2]);
-
-	while(i<strlen(whole)){ /* Sign compare warning here, please fix it */
-
-		/* Looks for i.4cdn in webpage's source */
-		if(whole[i-2]=='/'
-		&& whole[i-1]=='/'
-		  && whole[i]=='i'
-		&& whole[i+1]=='.'
-		&& whole[i+2]=='4'
-		&& whole[i+3]=='c'
-		&& whole[i+4]=='d'
-		&& whole[i+5]=='n')
-		{
-			/* Writes image's URL to 'link' */
+	
+	/* Sign compare warning fix */
+	thrlen=strlen(whole);
+	
+	while(i<thrlen){ 
+		/* Looks for '"ext":' */
+		if(whole[i-7]=='"'
+		&& whole[i-6]=='e'
+		&& whole[i-5]=='x'
+		&& whole[i-4]=='t'
+		&& whole[i-3]=='"'
+		&& whole[i-2]==':'
+		&& whole[i-1]=='"')
+		{	
+			/* Dirty webm workaround */			
+			extension[4]='\0';
+			
 			while(whole[i+j]!='"'){
-				link[j]=whole[i+j];
+				extension[j]=whole[i+j];
 				j++;
 			}
-
+			j=0;
+		}
+		
+		/* Looks for '"tim":' */
+		if(whole[i-6]=='"'
+		&& whole[i-5]=='t'
+		&& whole[i-4]=='i'
+		&& whole[i-3]=='m'
+		&& whole[i-2]=='"'
+		&& whole[i-1]==':')
+		{
+			/* Writes image's number to 'imagenumber' */
+			while(whole[i+j]!=','){
+				imagenumber[j]=whole[i+j];
+				j++;
+			}
+			
 			/* Downloads the image */
 			count++;
-			printf("\nDownloading image %d: %s...",count,link);
-			sprintf(wget,"wget -nc -P %s -q %s ",buff,link);
+			printf("\nDownloading image %d: i.4cdn.org/%s/src/%s%s...",count,boardname,imagenumber,extension);
+			sprintf(wget,"wget -nc -P %s -q i.4cdn.org/%s/src/%s%s",buff,boardname,imagenumber,extension);
 			system(wget);
 			printf(" Done!");
 			i+=180;
-
-			/* Webm workaround */
-			if (link[strlen(link)-1]=='m'){
-				link[strlen(link)-1]='\0';}
-
-			/* Broken link workaround */
-			if (strlen(link)>40){
-				sprintf(link, " ");
-				count--;
-			}
 		}	
 		i++;j=0;
 	}
